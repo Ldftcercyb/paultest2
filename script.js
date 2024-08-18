ce3697c75c14da46c9bb41e04ab3e88788f83d60
Ldftcer-Elbakyan-homewoork-classrom <156641760+Ldftcercyb@users.noreply.github.com>
2024-08-18 11:09:22 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// document.write('<table style="width:800px;height:800px; border:2px solid black">')
+// for (let i = 0; i <= 7; i++) {
+//     document.write('<tr style=" width:100px; height:100px; ">')
+//     for (let a = 0; a <= 7; a++) {
+//         if (a % 2 == 1){
+//             document.write('<td style="width:100px;height:100px; background-color:black">')
+
+//         }else(
+//             document.write('<td style="width:100px;height:100px; background-color:white">')
+
+//         )
+
+//         document.write('</td>')
+//     }
+//     document.write('</tr>')
+
+// }
+// document.write('</table>')
+// document.write(
+//     '<table style="width:800px;height:800px;border:1px solod black;">'
+// );
+// for (let i = 0; i < 8; i++) {
+//     document.write(
+//         '<tr style="width:100px;height:100px;border:1px solid black">'
+//     );
+//     for (let a = 0; a < 8; j++) {
+//         if (i % 2 ==0&& j % 2==0) {
+//             document.write(
+//                 '<td style="width:50px;height:50px;border:1px solid black;background:black">'
+//             );
+//         } else
+//             document.write(
+//                 '<td style="width:50px;height:50px;border:1px solid black;background:white">'
+//             );
+//         document.write("</td>");
+//     }
+//     document.write("</tr>");
+// }
+// document.write("</td>");
+// document.write("</table>")
+
